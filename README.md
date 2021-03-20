4c78d00f124f5b384c35069830e3e4c272673586
Leandro Sena <leo.sena777@gmail.com>
2021-03-20 18:37:50 -0300

    adds

---
+++ README.md
@@ -0,0 +1,5 @@
+# github explorer
+
+Simple project configuration with typescript, webpack and babel.
+
+<img src="./doc/print.png" width="400">
--- src/components/RepositoryItem.jsx
+++
@@ -1,9 +0,0 @@
-export function RepositoryItem() {
-  return (
-    <li>
-      <strong>unform</strong>
-      <p>Forms in React js</p>
-      <a href="#"> Acessar repositório</a>
-    </li>
-  );
-}
